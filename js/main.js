878fe5a004f3179bb5d2a3de4142ae47ecccac8e
adil tahiri <rajaadil1995@gmail.com>
2022-03-16 18:19:19 +0100

    update the website

--- js/main.js
+++ js/main.js
@@ -1,46 +1,65 @@
 const responsive = {
   0: {
-    items: 1
+    items: 1,
   },
   320: {
-    items: 1
+    items: 1,
   },
   560: {
-    items: 2
+    items: 2,
   },
   960: {
-    items: 3
-  }
+    items: 3,
+  },
 };
 
-$(Document).ready(function() {
-  $nav = $('.nav');
-  $toggleCollapse = $('.toggle-collapse');
+$(Document).ready(function () {
+  $nav = $(".nav");
+  $toggleCollapse = $(".toggle-collapse");
 
   // click even on toggle menu
-  $toggleCollapse.click(function() {
-    $nav.toggleClass('collapse');
+  $toggleCollapse.click(function () {
+    $nav.toggleClass("collapse");
   });
   // owl carousel for blog
-  $('.owl-carousel').owlCarousel({
+  $(".owl-carousel").owlCarousel({
     loop: true,
     autoplay: true,
     autoplayTimeout: 3000,
     dots: false,
     nav: true,
     navText: [
-      $('.owl-navigation .owl-nav-prev'),
-      $('.owl-navigation .owl-nav-next')
+      $(".owl-navigation .owl-nav-prev"),
+      $(".owl-navigation .owl-nav-next"),
     ],
-    responsive: responsive
+    responsive: responsive,
   });
-  // click to scroll top
-  $('.move-up span').click(function() {
-    $('html,body').animate(
-      {
-        scrollTop: 0
-      },
-      1000
-    );
+  // $(window).scroll(function () {
+  //   if ($(this).scrollTop() > 100) {
+  //     $(".move-up span").fadeIn();
+  //   } else {
+  //     $(".move-up span").fadeOut();
+  //   }
+  // });
+  // // click to scroll top
+  // $(".move-up span").click(function () {
+  //   $("html,body").animate(
+  //     {
+  //       scrollTop: 0,
+  //     },
+  //     1000
+  //   );
+  // });
+});
+let upTuTop = document.querySelector(".move-up span");
+window.onscroll = function () {
+  this.scrollY >= 200
+    ? upTuTop.classList.add("show")
+    : upTuTop.classList.remove("show");
+};
+upTuTop.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
   });
 });
